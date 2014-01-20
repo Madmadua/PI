4476a9372e1bb750039c3f62c55a45cb1c0a591a
Pierre Pézot <pierre.pezot@telecomnancy.net>
2014-01-20 15:42:39 +0100

    sctk metric

---
+++ metrics/openhart/sctk/sctk_0.1.php
@@ -0,0 +1,278 @@
+<?php
+
+/**
+ * @file algowsdl.php
+ * 
+ * @author  Yingjie Li <yil308@lehigh.edu>
+ * @author  Bart Lamiroy <lamiroy@cse.lehigh.edu>
+ * 
+ * @date October 2010
+ * 
+ * @version 1.0
+ *
+ * @section Main Description
+ * 
+ * This file is the principal stub for WSDL based Web Service providing.
+ * It should be taken as a template for adaptation and extension of local
+ * execution of DAE related services.
+ * 
+ * @section Usage - Disclaimer
+ * 
+ * The only part of this code that needs to be modified is the \a callback()
+ * function. All other parts of this code should be left untouched. Relying on
+ * personalized modification of the rest of the code may result in loss of 
+ * compatibility with subsequent updates and evolutions of this code.
+ *   
+ * @section LICENSE
+ * 
+ *  This file is part of the DAE Platform Project (DAE).
+ *
+ *  DAE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DAE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DAE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @mainpage
+ * 
+ * @section Contents
+ * 
+ * This project is composed of 3 files:
+ *  - ocrad.php
+ *  - client.php
+ *  - setup.php
+ * 
+ * @section Note
+ * 
+ * For demo executions, and if the \a $algoname variable is modified in setup.php, this file
+ * should be renamed to \a $algoname.php.
+ */
+ 
+//== START EDITABLE ZONE 
+
+/**
+ * \brief Name of hosted algorithm.
+ *  
+ * Should exactly match SQL query "select NAME from ALGORITHM where ID = \a $algoOracleID"
+ * 
+ * @var $algoname 
+ */
+$algoname = 'sctk';
+$algoversion = '0.1';
+
+/**
+ * Edit the contents of setup.php to fit your needs.
+ */ 
+include('setup.php');
+
+/**
+ * Array of input variables for hosted algorithm.
+ * 
+ * \todo write an extensive explanation on how to format this part
+ * 
+ * IMPORTANT: due to SOAP restrictions, the 'name' string cannot contain whitespaces. 
+ * This is an important constraint, given the assumed correspondence with the Oracle
+ * data or data_type mapping.
+ * 
+ * @var $inputT
+ */
+$inputT = array();
+$inputT['reference-url'] = array('name'=>'reference-url', 'type'=>'xsd:string');
+$inputT['hypothesis-url'] = array('name'=>'hypothesis-url', 'type'=>'xsd:string');
+
+/**
+ * Array of output variables for hosted algorithm.
+ * 
+ * \todo write an extensive explanation on how to format this part
+ * 
+ * IMPORTANT: due to SOAP restrictions, the 'name' string cannot contain whitespaces. 
+ * This is an important constraint, given the assumed correspondence with the Oracle
+ * data or data_type mapping.
+ * 
+ * @var $outputT
+ */
+$outputT['result-url'] = array('name'=>'result-url', 'type'=>'xsd:string');
+$outputT['summary-url'] = array('name'=>'summary-url', 'type'=>'xsd:string');
+//== STOP EDITABLE ZONE
+
+/**
+ * This function is currently a demo stub running 'Ocrad'. Eventually this will launch the 
+ * effective execution of the corresponding algorithm (as set in the \a $input URIs array parameter)
+ *  
+ * @param [in] $input the array of arguments related to the execution
+ * 
+ * The exact input and output parameters of this function are defined by the \a $inputT and \a $outputT variables in \a setup.php.
+ * 
+ */
+function callback($input) {
+
+	global $htmlBaseDir;
+	global $executionDir;
+	global $executionPrefix;
+	global $algoname;
+    global $hostname;
+	
+	// Creating temporary work directory
+	$localdir = $htmlBaseDir . '/' . $executionDir . '/' . $algoname . '/' . time();
+	//$localdir='wsdl';
+	if (!is_dir($localdir) && ! mkdir($localdir,0777,true)) {
+	    return new soap_fault('SERVER', '', 'Cannot create local directory', '');
+	}
+	chmod($localdir,0777);
+	
+	//== START EDITABLE ZONE
+	/*
+	 * The following editable zone is made for retrieving the input parameters
+	 * and preparing them (copying on disk, other preparatory manipulations) to
+	 * be provided as input arguments to the actual executable.
+	 * 
+	 * In this case there is only one input parameter: the input file
+	 * 
+	 * ATTENTION! When modifying this code, be sure the array keys correspond to 
+	 * the keys declared in \a $inputT in setup.php
+	 */
+	$inputReference = $input['reference-url'];
+	$inputHypothesis = $input['hypothesis-url'];
+	
+	/*
+	 * This is code is only doing raw execution without the required logging
+	 * on the DAE server. Logging provenance on the DAE server can only
+	 * be done when the software and service are dully registered with
+	 * the platform.
+	 */ 
+	
+
+    //Getting reference file  
+    
+	$referenceFile = $localdir.'/'.array_pop(explode("/",$inputReference));
+    $execString = 'wget '.$inputReference.' -q -O '.$referenceFile;
+    //$execString = 'cp '.$inputReference.' '.$localdir.'/'.$inputReference;
+    system($execString, $returnCode);
+	if ($returnCode) {
+	    return new soap_fault('SERVER', '', 'Execution Error', 'Return code = ' . $returnCode .' '. $execString);
+	}
+
+    //Getting hypothesis file
+
+	$hypothesisFile = $localdir.'/'.array_pop(explode("/",$inputHypothesis));
+    //$execString = 'cp '.$inputHypothesis.' '.$localdir.'/'.$inputHypothesis;
+	$execString = 'wget '.$inputHypothesis.' -q -O '.$hypothesisFile;
+	system($execString, $returnCode);
+	if ($returnCode) {
+	    return new soap_fault('SERVER', '', 'Execution Error', 'Return code = ' . $returnCode .' '. $execString);
+	}
+
+    //Running tercom
+
+	//$execString = 'java -Xmx2G -jar bin/meteor-1.4.jar '.$hypothesisFile.' '.$referenceFile.' -m \'exact stem synonym\' -l en > '.$localdir.'/output.txt';
+    $execString = './bin/sclite -r '.$referenceFile.' -h '.$hypothesisFile.' -i sp -e utf-8 -D -o rsum \"sgml\" -n output -O '.$localdir.' 1>/dev/null 2>/dev/null';
+    //== STOP EDITABLE ZONE
+	
+	$returnValue = system($execString,$returnCode);
+	if ($returnCode) {
+	    return new soap_fault('SERVER', $error, 'Execution Error', 'Return code = ' . $returnCode .' '. $execString);
+	}
+
+	//== START EDITABLE ZONE
+	/*
+	 * This editable zone retrieves the results from the execution and creates the appropriate
+	 * output data structure for retrieval by the caller.
+	 * 
+	 * In this particular case, the execution output consists of 2 files that have been stored
+	 * in the execution directory. Since this directory is accessible by the webserver, we construct
+	 * the appropriate URLs so that the client can download them.
+	 */
+	// Stripping absolute path information and prefixing URL information.
+	//$localOCR = 'http://'.$hostname. substr($localOCR,strlen($htmlBaseDir));
+	//$localLayout = 'http://'.$hostname. substr($localLayout,strlen($htmlBaseDir));
+		
+	// ATTENTION! Be sure the array keys correspond to the keys declared in \a $outputT in setup.php
+	/*$result = array(
+   	'ocr_result_file' => $localOCR, 
+       	'layout_result_file' => $localLayout
+    );*/
+    $result=array('result-url' => 'http://localhost/'.substr($localdir,9).'/output.sgml','summary-url' => 'http://localhost/'.substr($localdir,9).'/output.raw');
+    //$result=array('result-url' => 'http://localhost/wsdl/i');
+    //== STOP EDITABLE ZONE
+    
+    return $result;
+}
+
+/*
+ * This PHP page genereates WSDL files for different algorithms with different inputs and outputs.
+ */
+ 
+// Pull in the NuSOAP code
+require_once('lib/nusoap.php');
+
+/**
+ * Publishes a WSDL service for the algorithm defined in \a setup.php. 
+ * It's WSDL interface description is returned. If not, it is supposed to be combined 
+ * with an HTTP_POST upload of the required input data.
+ * 
+ * \todo UDDI registration on the DAE server.
+ * 
+ * THIS CODE SHOULD NOT BE MODIFIED WHEN ADAPTED TO SOME LOCAL SERVICE. 
+ * ALL CODE MODIFICATION, CONFIGURATION AND ADAPTATION SHOULD BE DONE IN EITHER
+ * \a setup.php OR IN THE ABOVE \a callback() FUNCTION.
+ * 
+ * Modification of the code below may result in incompatibility issues or difficulties
+ * to upgrade to newer versions.
+ */
+ 
+// Create the server instance
+$server = new soap_server();
+
+// Retrieve the path of the algorithm to be invoked
+//$algopath = $db->r("select PATH from ALGORITHM where ID = ".$algoid);
+	
+// Initialize WSDL support
+$server->configureWSDL($algoname.'wsdl', 'urn:'.$algoname.'wsdl');
+
+/*
+ * Generate an array of input parameters as resulting from the following query
+ * "select NAME from DATATYPE where ID in (	select DATATYPE_ID from TYPE_OF where DATA_ID in (
+ *				select DATA_ID from ALGORITHM_INPUT where ALGORITHM_ID = ". $algoid ."	))"
+ *
+ * This array (\a $inputT) is defined in \a setup.php
+ *  
+ */ 
+$server->wsdl->addComplexType('inputType_'.$algoname, 'complexType',	'struct',
+							  'all', '', $inputT);
+
+/*
+ * Generate an array of output parameters as resulting from the following query
+ * "select NAME from DATATYPE where ID in (	select DATATYPE_ID from TYPE_OF where DATA_ID in (
+ *				select DATA_ID from ALGORITHM_OUTPUT where ALGORITHM_ID = ". $algoid ."	))" 
+ *
+ * This array (\a $outputT) is defined in \a setup.php
+ *  
+ */ 
+$server->wsdl->addComplexType('outputType_'.$algoname, 'complexType', 'struct', 
+							  'all', '', $outputT);
+
+// Register the method to expose
+$server->register('callback',                	// method PhP callback function
+		array('args' => 'tns:inputType_'.$algoname),  	//input parameters
+		array('return' => 'tns:outputType_'.$algoname), // output parameters
+		'urn:'.$algoname.'wsdl',				// namespace              
+		'urn:'.$algoname.'wsdl#'.$algoname,		// soapaction
+   		'rpc',                                	// style
+   		'encoded',                            	// use          
+		$algoname.' algorithm'					// documentation
+);
+
+
+// Use the POST request to (try to) invoke the service
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$server->service($HTTP_RAW_POST_DATA); 
+?>
---
+++ metrics/openhart/sctk/setup.php
@@ -0,0 +1,233 @@
+<?php 
+
+/**
+ * @file setup.php
+ * 
+ * @author  Bart Lamiroy <lamiroy@cse.lehigh.edu>
+ * 
+ * @date October 2010
+ *  
+ * @version 1.0
+ * 
+ * \brief  Global variable settings for demo purposes.
+ * 
+ * \section DAEParams DAE Related Parameters
+ * 
+ *  DAE Related Parameters are those that allow to relate the provided service to
+ *  a DAE registered algorithm. The are:
+ *   - $algoOracleID
+ *   - $algoname
+ *   - $inputT
+ *   - $outputT
+ *   - $WSDLURL
+ * 
+ *  Values of these variables should be defined with extreme caution since they can
+ *  impact global performances of the DAE process chain. 
+ * 
+ * \section LocalParams Local Webserver Parameters
+ * 
+ *  Local Webserver Parameters are:
+ *   - $htmlBaseDir
+ *   - $executionDir
+ *   - $executionPrefix
+ * 
+ */
+
+/**
+ * Base directory for local webserver data. This directory is needed for storing 
+ * final results of an execution and in order to allow results to be downloaded.
+ *  
+ * @var $htmlBaseDir
+ */
+$htmlBaseDir = '/var/www';
+
+/**
+ * Subdirectory name (relative to \a $htmlBaseDir) where final execution results will be
+ * stored. Each individual execution will create a unique subdirectory under \a $executionDir.
+ * 
+ * @var $executionDir
+ */
+$executionDir = 'openhart/sctk';
+
+/**
+ * Filename prefix to be used for storing intermediate results. Can be left blank. 
+ * 
+ * @var $executionPrefix
+ */
+$executionPrefix = 'openhart_';
+
+/**
+ * \brief URL that will host the provided service.
+ *
+ * @var $WSDLURL
+ */
+$domainname = '';
+$hostname = 'localhost';
+$algoname = 'sctk';
+$algoversion = '0.1';
+$WSDLURL = "http://$hostname/$executionDir/$algoname"."_$algoversion.php"; //used in client.php
+
+/**
+ * USE CAUTION ! Possible password exposure. 
+ * 
+ *  By setting this variable you will be able to download data from the DAE server 
+ *  
+ *  The source code transmitting your username/password does not verify whether it is actually
+ *  transmitting them
+ * 
+ * @var $DAEUsername
+ * @var $DAEPassword
+ */
+$DAEUsername = 'your-username';
+$DAEPassword = 'your-password';
+
+/* ============================================================================== */
+
+/* 
+ * NO EDITING REQUIRED BEYOND THIS ZONE !
+ * 
+ */
+
+/* 
+*/
+
+/**
+ * 
+ * This function connects to the URL using a POST command 
+ * and retrieves only the returned header information.
+ * 
+ * In the event of the URL server name to be dae.cse.lehigh.edu
+ * the function will submit connection information based on the
+ * global defined $DAEUsername $DAEPassword.
+ * 
+ * @param [in] $url the URL from which to retrieve header information
+ * 
+ * @return a string containing all HTTP header information.
+ * 
+ * This code comes from and has been adapted from
+ * http://forums.digitalpoint.com/showthread.php?t=1029916#post9241653
+ * 
+ */
+function HTTP_Get_Header( $url ) {
+    
+    /*
+     * These variables were declared in the general variable declaration
+     * section of this file.
+     */
+    global $DAEUsername;
+    global $DAEPassword;
+    
+    $get_timeout   = 40;
+    $s_Complete    = parse_url($url);
+    if( !isset($s_Complete["scheme"] ) ) {
+      $s_Complete["host"] = $s_Complete["path"];
+      $s_Complete["path"] = '';
+    }
+    $s_Host        = $s_Complete["host"];
+    if( @$s_Complete["path"] == "" )
+      $s_Complete["path"] = "/?";
+    $s_URI         = $s_Complete["path"];
+    if( @$s_Complete["query"] != "" )
+      $s_URI      .= '?'.$s_Complete['query'];
+    if( @$s_Complete["port"] != "" )
+      $s_Port      = $s_Complete["port"];
+    else
+      $s_Port      = 80;
+      
+    if(!strcmp('dae.cse.lehigh.edu',$s_Host)) {
+        $requestMsg = 'name='.urlencode($DAEUsername).'&pass='.urlencode($DAEPassword).'&op=Log%20in&form_id=user_login_block';
+    }
+    else { 
+        $requestMsg ='';
+    }
+    $requestLength = strlen($requestMsg);
+    
+    $request       = "POST $s_URI HTTP/1.0\r\n";
+    $request      .= "Accept: */*\r\n";
+    $request      .= "Host: $s_Host\r\n";
+    $request      .= "Connection: Close\r\n";
+    $request      .= "Content-Type: application/x-www-form-urlencoded\r\n";
+    $request      .= "Content-Length: $requestLength\r\n";
+    $request      .= "\r\n";
+    $request	  .= $requestMsg;
+    $fp = @fsockopen( $s_Host, $s_Port, $errno, $errstr, 30 );
+
+    if( is_resource($fp) ) {
+        fputs( $fp, $request );
+        $query_timeout = 30;
+        stream_set_blocking( $fp, true );
+        stream_set_timeout( $fp, $query_timeout );
+        $loop_time = time();
+        $status = socket_get_status( $fp );
+        $line = "";
+        $header = "";
+
+        while( !($line == "\r\n")  && !feof($fp) && !$status['timed_out'] ) {
+            $line = fgets($fp, 4096);
+            $header .= $line;
+            $diff = time() - $loop_time;
+
+            if( $diff > $get_timeout )
+            break;
+
+            if( connection_aborted() )
+            break;
+
+            $status = socket_get_status( $fp );
+        }
+
+        fclose( $fp );
+        return $header;
+    }
+
+    return false;
+}
+
+/**
+ *
+ */
+function DAEAuthCopy($source, $dest) {
+
+    $header = HTTP_Get_Header($source);
+        
+    if($header) {
+        $beginPos = strrpos($header,'Set-Cookie');
+        $header = substr($header, $beginPos+4);
+        $endPos = strpos($header,';');
+        $header = substr($header, 0, $endPos);
+
+        if($header) {
+            $opts = array(
+  				'http'=>array(
+    			'method'=>"GET",
+    			'header'=>"Accept: */*\r\n" .
+	          	  	  "Connection: Keep-alive\r\n" .
+	          	  	  "$header\r\n"            
+                  )
+             );
+            $context = stream_context_create($opts);
+            $fp = fopen($source,'r',false,$context);
+        } else {
+            $fp = fopen($source,'r');
+        }
+
+        $fpout = fopen($dest,'w');
+    
+        if(!$fp || !$fpout ) return false;
+    
+        while (!feof ($fp)) {
+               $line = fread ($fp, 4092);
+               fwrite($fpout,$line);
+        } 
+
+        fclose($fpout);
+        fclose($fp);
+    
+        return true;
+    }
+    else {
+        return copy($source,$dest);
+    }
+}
+
+?>
